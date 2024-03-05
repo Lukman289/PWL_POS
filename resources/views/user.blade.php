a89d62bad19a23b2c9dbeca18ebcbf51fbbe1d49
Lukman Eka Septiawan <lukmaneka216@gmail.com>
2024-03-05 13:43:50 +0700

    Jonsheet 4_Praktikum 2.5

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -10,11 +10,11 @@
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>Jumlah Pengguna</th>
-            {{-- <th>ID</th>
+            {{-- <th>Jumlah Pengguna</th> --}}
+            <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
-            <th>ID Level Pengguna</th> --}}
+            <th>ID Level Pengguna</th>
         </tr>
         {{-- @foreach ($data as $d)
             <tr>
@@ -25,11 +25,11 @@
             </tr>
         @endforeach --}}
             <tr>
-                <td> {{$data}} </td>
-                {{-- <td> {{$data->user_id}} </td>
+                {{-- <td> {{$data}} </td> --}}
+                <td> {{$data->user_id}} </td>
                 <td> {{$data->username}} </td>
                 <td> {{$data->nama}} </td>
-                <td> {{$data->level_id}} </td> --}}
+                <td> {{$data->level_id}} </td>
             </tr>
     </table>
 </body>
