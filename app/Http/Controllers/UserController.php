cb6c1494a87202f60089dde4748f1fc109f9da24
Lukman Eka Septiawan <lukmaneka216@gmail.com>
2024-03-08 17:12:27 +0700

    Jobsheet 4_Praktikum 2.7 Relationship

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -135,7 +135,11 @@ class UserController extends Controller
         // $user->wasChanged('nama'); // false
         // dd($user->wasChanged(['nama', 'username'])); // false
 
-        $user = UserModel::all();
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::with('level')->get();
+        // dd($user);
         return view('user', ['data' => $user]);
     }
 
