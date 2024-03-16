6c9c35b82070ad798273f61a68438f462f766adf
Lukman Eka Septiawan <lukmaneka216@gmail.com>
2024-03-16 20:07:36 +0700

    Jobsheet 5_Tugas 2

--- config/adminlte.php
+++ config/adminlte.php
@@ -334,6 +334,11 @@ return [
             'icon' => 'fas fa-fw fa-lock',
         ],
         [
+            'text' => 'Manage_Kategori',
+            'url' => 'kategori',
+            'icon' => 'fas fa-fw fa-bars',
+        ],
+        [
             'text' => 'multilevel',
             'icon' => 'fas fa-fw fa-share',
             'submenu' => [
